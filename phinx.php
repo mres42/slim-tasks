35263fbcf607b53389e34a587c9cb4a3e5452367
mres42 <mario.github@protonmail.com>
2025-12-12 22:44:52 -0300

    add migration support

---
+++ phinx.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use Dotenv\Dotenv;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+    'paths' => [
+        'migrations' => 'database/migrations',
+        'seeds'      => 'database/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_NAME'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'port' => 3306,
+            'charset' => 'utf8mb4',
+        ]
+    ]
+];
